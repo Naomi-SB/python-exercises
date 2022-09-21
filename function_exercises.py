c5f0d513766e2aaf18982f65929643af61042a57
Naomi Sweatt <sweatt.b.naomi@gmail.com>
2022-09-21 12:07:31 -0500

    exercises completed and correct

--- function_exercises.py
+++ function_exercises.py
@@ -5,17 +5,40 @@ def is_two(number):
     else:
         return False
 
+#could simplify to : 
+def is_two(n):
+    return n == 2 or n == '2'
+
 def is_vowel(letter):
     if letter == ['a'] or letter == ['e'] or letter == ['i'] or letter == ['o'] or letter == ['u']:
         return True
     else: 
         return False
-
+# checking for more conditions:
+def is_vowel(somestring):
+    if type (somestring) == str:
+        if len(somestring) ==1:
+            return somestring.lower() in list ('aeiou')
+        else:
+            return False
+    else: 
+        return False
+        
 def is_consonant(letter):
     if letter == ['a'] or letter == ['e'] or letter == ['i'] or letter == ['o'] or letter == ['u']:
         return False
     else: 
         return True
+# Andrew's way:
+def is_consonant(somestring):
+    if type(somestring) == str:
+        if len(somestring) == 1 and somestring.isalpha():
+            return(not is_vowel(somestring))
+        else:
+            return False
+    else:
+        return False
+
 
 def remove_vowels(word):
   vowels = ('a', 'e', 'i', 'o', 'u')
@@ -44,13 +67,24 @@ def apply_discount():
   amount_discounted = og_price * dc_percentage
   return "The new cost is: ", (og_price - amount_discounted)
 
+#Andrew's much simpler way:
+def apply_discount(orig_price, discount = 0.0):
+    return orig_price - orig_price*discount
+
 def handle_commas(number):
   no_commas = number.split(',')
   all_together_now = (''.join(no_commas))
   return int(all_together_now)
+#Andre used replace
+def handle_commas(n):
+    if type(n) == str:
+        return int(n.replace(',', ''))
+    else:
+        return n
 
 while True:
-    get_letter_grade = int(input("enter a numerical grade 0-100"))
+def get_letter_grade(user_grade)
+    user_grade = int(input("enter a numerical grade 0-100"))
 
     if user_grade >= 88:
         print('A')
@@ -63,3 +97,31 @@ while True:
     else:
         print('F')
 
+
+def remove_vowels(word):
+    somestring = ''
+    for letter in word:
+        if not is_vowel(letter):
+            somestring += letter
+    return somestring
+
+def normalize_name(name):
+  newstring = ''
+  #for loop to grab all letter, numbers, and spaces
+  for letter in name:
+    if letter.isalnum() or letter == ' ':
+      newstring += letter
+    # for loops to check if leading character is a letter
+    for letter in newstring:
+        if letter.isalpha():
+            break
+        else:
+            newstring = newstring[1:]
+  return newstring.strip().lower().replace(' , '_')
+
+def cumulative_sum(oldlist):
+  newlist = []
+  for i in range(1, len(oldlist)+1):
+    cumusum = sum(oldlist[:i])
+    newlist.append(cumusum)
+  return newlist
